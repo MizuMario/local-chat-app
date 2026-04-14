1c01c891004c6a17f07a3579e154064692585986
Mizu_Mario <22jh28@gmail.com>
2026-04-14 19:34:16 +0900

    Initial Commit

---
+++ local-chat-app/local_chat_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  local_chat_appApp.swift
+//  local-chat-app
+//
+//  Created by mizuu on 2026/04/14.
+//
+
+import SwiftUI
+
+@main
+struct local_chat_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
